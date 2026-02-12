33aaf688aee35f16297704bb7523f9d61bc52705
Andres Mendez <a.mendez.g@cofana-labs.com>
2026-02-12 16:32:29 +0100

    feat: Integrate Firebase Remote Config to dynamically load

---
+++ src/firebase/client.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAAYW1eGIQUSjgiMwHGoun_hF71NfkSetQ",
+  authDomain: "padawan-portfolio.firebaseapp.com",
+  projectId: "padawan-portfolio",
+  storageBucket: "padawan-portfolio.firebasestorage.app",
+  messagingSenderId: "831376796485",
+  appId: "1:831376796485:web:43b8769460de5716f6ec2c"
+};
+
+export const app = initializeApp(firebaseConfig);
