e4a2de4a76ad10da9ad2571275be3e511e0ebf13
Andres Mendez <a.mendez.g@cofana-labs.com>
2026-03-27 20:22:18 +0100

    feat: update env.d.ts with new mail service environment variables and add ContactResponse interface

--- src/env.d.ts
+++ src/env.d.ts
@@ -10,8 +10,12 @@ interface ImportMetaEnv {
   readonly FIREBASE_CLIENT_CERT_URL: string;
   readonly BASE_URL: string;
   readonly PUBLIC_RECAPTCHA_URL: string;
+  readonly RECAPTCHA_SITE_KEY: string;
   readonly RECAPTCHA_SECRET_KEY: string;
   readonly PUBLIC_BACKEND_URL: string;
+  readonly PUBLIC_MAIL_SERVICE_ID: string;
+  readonly PUBLIC_MAIL_TEMPLATE_ID: string;
+  readonly PUBLIC_MAIL_PUBLIC_KEY: string;
 }
 
 interface ImportMeta {
