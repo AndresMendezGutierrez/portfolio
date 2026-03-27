9754216c0e58ff5067a099cf5c0e07e214a8dfa7
Andres Mendez <a.mendez.g@cofana-labs.com>
2026-03-27 20:21:22 +0100

    feat: implement sendEmail function for email sending functionality

---
+++ src/services/mail/mail.service.ts
@@ -0,0 +1,21 @@
+import emailjs from "@emailjs/browser";
+import type { ContactResponse } from "../types";
+
+export const sendEmail = async (form: HTMLFormElement): Promise<ContactResponse> => {
+  try {
+    const result = await emailjs.sendForm(
+      import.meta.env.PUBLIC_MAIL_SERVICE_ID,
+      import.meta.env.PUBLIC_MAIL_TEMPLATE_ID,
+      form,
+      import.meta.env.PUBLIC_MAIL_PUBLIC_KEY,
+    );
+
+    return { success: result.status === 200 };
+  } catch (error) {
+    console.error("Mail Error:", error);
+    return {
+      success: false,
+      errorType: "error_mail",
+    };
+  }
+};
