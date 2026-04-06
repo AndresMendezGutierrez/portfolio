37dee88fa36fb592646eea6d5a1e66f490cba159
Andres Mendez <a.mendez.g@cofana-labs.com>
2026-04-06 09:52:23 +0200

    feat: clean up Firebase environment variables by removing unused keys

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,19 +3,12 @@
 import type { ToastAPI, GrecaptchaAPI } from "./types";
 
 interface ImportMetaEnv {
-  readonly FIREBASE_PRIVATE_KEY_ID: string;
   readonly FIREBASE_PRIVATE_KEY: string;
   readonly FIREBASE_PROJECT_ID: string;
   readonly FIREBASE_CLIENT_EMAIL: string;
-  readonly FIREBASE_CLIENT_ID: string;
-  readonly FIREBASE_AUTH_URI: string;
-  readonly FIREBASE_TOKEN_URI: string;
-  readonly FIREBASE_AUTH_CERT_URL: string;
-  readonly FIREBASE_CLIENT_CERT_URL: string;
   readonly BASE_URL: string;
   readonly PUBLIC_RECAPTCHA_URL: string;
   readonly RECAPTCHA_SITE_KEY: string;
-  readonly RECAPTCHA_SECRET_KEY: string;
   readonly PUBLIC_BACKEND_URL: string;
   readonly PUBLIC_MAIL_SERVICE_ID: string;
   readonly PUBLIC_MAIL_TEMPLATE_ID: string;
